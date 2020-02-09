24b92bc66a1c890b4547485a40785e0a6dab88e8
dsqrt4 <44970945+dsqrt4@users.noreply.github.com>
2020-02-09 17:56:48 +0100

    move some code

--- current/executor.go
+++ current/executor.go
@@ -1,25 +1,18 @@
 package current
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"strings"
 )
 
 type Executor func(query url.URL) (io.Reader, error)
 
 var DefaultExecutor Executor = func(query url.URL) (data io.Reader, err error) {
-	request, err := http.NewRequest(http.MethodGet, query.String(), nil)
-	if err != nil {
-		return nil, err
-	}
-	request.Header.Set("accept", "application/json")
-
-	c := http.Client{}
-	r, err := c.Do(request)
+	r, err := getResponse(query)
 	if err != nil {
 		return nil, err
 	}
@@ -34,5 +27,16 @@ var DefaultExecutor Executor = func(query url.URL) (data io.Reader, err error) {
 		return nil, err
 	}
 
-	return strings.NewReader(string(body)), nil
+	return bytes.NewReader(body), nil
+}
+
+func getResponse(query url.URL) (*http.Response, error) {
+	request, err := http.NewRequest(http.MethodGet, query.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Set("accept", "application/json")
+
+	c := http.Client{}
+	return c.Do(request)
 }
